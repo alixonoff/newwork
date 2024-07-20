c52ddf1ea786f7577397f5b6e13488d10464de24
alixonoff <alixonakbaraliyev76@gmail.com>
2024-07-21 00:31:54 +0500

    lorem

---
+++ index.js
@@ -0,0 +1,34 @@
+// alert ("Hallo react");
+// let resalt = prompt("yoshingzni kiriting: ")
+// console.log(resalt);
+
+// tasdiqlash uchun ishlatiladi 
+// let resalt = confirm("Tasdiqlashni qiling: ");
+// console.log(resalt);
+
+// let resalt = prompt("Yoshingizni kiriting: ");
+// console.log(resalt >=18);
+
+// type conversion bir ma’lumot turini boshqa ma’lumot turiga o’zgartirish 
+
+// NaN - not a number
+
+
+// number conversion
+let str = "123";
+let number = Number(str);
+console.log(number);
+// console.log(number);
+// console.log(number);
+// console.log(number);
+
+// String conversion
+let str2 = 15;
+let number2 = String(str2);
+console.log(str2);
+
+
+// let resal1 = prompt("Birinchi raqamni kiriting: ")
+// let resal2 = prompt("Ikkininchi raqamni kiriting: ")
+// let resal3 = prompt("Ikkininchi raqamni kiriting: ")
+// console.log(Number(resal1) + Number(resal2));
